891cef0c548e051677f20702db45a6d12ef15865
Maria-Belova122 <mkb_maria@mail.ru>
2024-10-12 17:36:44 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+# ЗАДАНИЕ ПО ТЕМЕ "Модули и пакеты"
+
+from true_math import divide as d1
+from fake_math import divide as d2
+
+result1 = d2(69, 3)
+result2 = d2(3, 0)
+result3 = d1(49, 7)
+result4 = d1(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
